832282d7bd81b80482b2cf0014ce6b83936af810
nash <n.tksh2@gmail.com>
2014-04-15 02:07:33 +0900

    Add common header

---
+++ src/common.h
@@ -0,0 +1,16 @@
+/*
+ * common.h
+ *
+ *  Created on: 2014/04/14
+ *      Author: nash
+ */
+
+#ifndef COMMON_H_
+#define COMMON_H_
+
+#include <iostream>
+#include <assert.h>
+
+
+
+#endif /* COMMON_H_ */
