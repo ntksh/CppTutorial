b92372630ba33c60a84ce8022e4f430919248eaa
nash <n.tksh2@gmail.com>
2014-04-15 02:25:06 +0900

    Add Integer

---
+++ src/integer.cpp
@@ -0,0 +1,19 @@
+/*
+ * integer.cpp
+ *
+ *  Created on: 2014/04/15
+ *      Author: nash
+ */
+
+class Integer {
+public:
+	int m_value;
+
+	Integer();
+
+};
+
+// constructor
+Integer::Integer() {
+	m_value = 0;
+}
